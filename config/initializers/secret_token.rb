4792e18bcc6dca10b9766740e16f9869a018e96e
Dan Silver <dannysilver3@gmail.com>
2013-09-21 00:04:24 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactsMap::Application.config.secret_token = 'b46ae2b6ff0c82259a9aeb454a74330fe81277834e05532034ea7466cb17ec55a75fca392c3ad0e302e30eacfd8e151efdf3e717216f84fee9161074d28b4389'
